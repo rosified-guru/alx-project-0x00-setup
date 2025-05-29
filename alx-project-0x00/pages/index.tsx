370e17dac981b4d571223e4d3c99d98fe0a175cf
Rose Mwangi <muthonirosee7@gmail.com>
2025-05-29 17:01:40 +0300

    add components and interfaces

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -11,8 +11,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+const Home: React.FC =()=> {
   return (
+      <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
+}
+
+export default Home;
+
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
@@ -111,5 +120,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
-}
+  ;
+
