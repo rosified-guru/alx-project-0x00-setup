dff267145361ac30152c06ec076e89b7ac70dfa4
Rose Mwangi <muthonirosee7@gmail.com>
2025-05-29 18:59:25 +0300

    Basic Routing

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -20,7 +21,7 @@ const Home: React.FC = () => {
       <h1>Airbnb Application Clone system</h1>
       <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
     </main>
-export default Home;
+
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -71,3 +72,4 @@ export default Home;
     </div>
   );
 }
+export default Home;
